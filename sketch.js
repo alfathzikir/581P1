752d2114ff80aa2e22cb06e7320b61e88b113845
alfathzikir <aifzikir@gmail.com>
2021-03-01 22:41:37 -0700

    test

--- sketch.js
+++ sketch.js
@@ -1,23 +1,12 @@
 function setup() {
-  createCanvas(710, 200);
-
-  // Create an Audio input
-  mic = new p5.AudioIn();
-
-  // start the Audio Input.
-  // By default, it does not .connect() (to the computer speakers)
-  mic.start();
+  createCanvas(400, 400);
 }
 
 function draw() {
-  background(200);
-
-  // Get the overall volume (between 0 and 1.0)
-  let vol = mic.getLevel();
-  fill(127);
-  stroke(0);
-
-  // Draw an ellipse with height based on volume
-  let h = map(vol, 0, 1, height, 0);
-  ellipse(width / 2, h - 25, 50, 50);
+  if (mouseIsPressed) {
+    fill(0);
+  } else {
+    fill(255);
+  }
+  ellipse(mouseX, mouseY, 80, 80);
 }
